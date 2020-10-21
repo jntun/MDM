b302ca36b056712939ce72a7d814fdf147f4b14d
Nastyyy <nerorazerx@gmail.com>
2020-10-21 16:39:20 -0700

    Need to make event logic for session. WIP

--- mdm-back/Main.go
+++ mdm-back/Main.go
@@ -5,48 +5,13 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/Nastyyy/mdm-back/market"
-	"github.com/gorilla/websocket"
 	uuid "github.com/satori/go.uuid"
 )
 
 const DEBUG bool = false
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
-
-func session(w http.ResponseWriter, r *http.Request, sess *market.Session) {
-	(w).Header().Set("Access-Control-Allow-Origin", "*")
-
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	for {
-		_, msg, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			break
-		}
-		handleMessage(msg, sess)
-	}
-}
-
-func handleMessage(byteMsg []byte, sess *market.Session) {
-	msg := NewMessage(byteMsg)
-	EventHandler(msg, sess)
-	fmt.Println(strings.Repeat("-", 80))
-}
 
 // Generates random UUID and writes response with it
 func authorize(w http.ResponseWriter, r *http.Request) {
@@ -89,6 +54,7 @@ func main() {
 	game := market.GameInstance{Running: true, TickRate: tickRate, Market: market.NewMarket()}
 	gameSession.SetGameInstance(&game)
 
+	// TODO: Possibly clean up and move to Game.go?
 	go func() {
 		fmt.Println("Starting market game...")
 		for game.Running {
@@ -101,9 +67,7 @@ func main() {
 	}()
 
 	fmt.Println("Starting HTTP server...")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		session(w, r, gameSession)
-	})
+	http.HandleFunc("/", gameSession.SocketHandler)
 	http.HandleFunc("/authorize", authorize)
 	http.HandleFunc("/getusername", getUsername)
 	s := &http.Server{
