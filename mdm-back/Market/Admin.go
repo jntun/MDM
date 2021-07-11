e9c53a4030446786bb411be2693045aa65267d45
jntun <justin.tunheim@gmail.com>
2021-07-10 21:24:47 -0700

    Admin: Detect if current admin is default and set user to new admin for sess

--- mdm-back/Market/Admin.go
+++ mdm-back/Market/Admin.go
@@ -1,9 +1,10 @@
 package market
 
 import (
+    "errors"
     "fmt"
-
     "github.com/Nastyyy/mdm-back/config"
+    "reflect"
 )
 
 type AdminAction struct {
@@ -12,8 +13,29 @@ type AdminAction struct {
 
 func (act AdminAction) DoAction(sess *Session, usr *User) error {
     config.VerboseLog(fmt.Sprintf("[ADMIN] Attempting match with %s - %s", sess.Admin.UUID.String(), usr.UUID))
+
+    if sess.Admin.Name == "default-admin" {
+    	config.DebugLog(fmt.Sprintf("No admin found, setting to %s..", usr.Name))
+        sess.Admin = usr
+    }
+
     if sess.Admin.UUID.String() == usr.UUID.String() {
-        config.DebugLog(fmt.Sprintf("Admin found: %s |\nBody: %s", usr.UUID, act.Body))
+        //config.DebugLog(fmt.Sprintf("Admin found: %s |\nBody: %s", usr.UUID, act.Body))
+        val, found := act.Body.(map[string]interface{})["admin-action"]
+        if !found {
+            return errors.New("No 'admin-action' field found.")
+        }
+
+        if reflect.TypeOf(val).Kind() != reflect.String {
+            return errors.New("Invalid 'admin-action' command.")
+        }
+
+        switch act.Body.(map[string]interface{})["admin-action"].(string) {
+        case "pause":
+            sess.Game.Stop()
+        case "resume":
+            sess.Game.Start()
+        }
         return nil
     }
 
